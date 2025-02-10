ef689481264a0972b0b84b51ef157200b04e994b
Gashpid <gizquierdorojas@gmail.com>
2025-02-10 06:58:49 -0500

    Update install and Add nginx config files

---
+++ install.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+############################
+# Update and Upgrade system
+############################
+echo "Installing system apps..."
+sudo apt update
+sudo apt upgrade -y
+
+
+########################
+# Install docker engine
+########################
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+$(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-compose -y
+
+
+################
+# Install Nginx
+################
+sudo apt install nginx -y
+
+
+####################################
+# Setup and configure your firewall
+####################################
+sudo ufw allow 22/tcp
+sudo ufw allow 80/tcp
+sudo ufw allow 443/tcp
+sudo ufw allow 8080/tcp
+sudo ufw allow 8444/tcp
+sudo ufw allow 3306/tcp
+sudo ufw allow 8080/tcp
+
+sudo ufw --force enable
+sudo ufw status verbose
+
+
+
+########################################
+# Copy Nginx config and enable services
+########################################
+sudo rm /etc/nginx/sites-enabled/default
+cp moodle.conf /etc/nginx/sites-available/moodle
+cp react-app.conf /etc/nginx/sites-available/react-app
+sudo ln -s /etc/nginx/sites-available/moodle /etc/nginx/sites-enabled/
+sudo ln -s /etc/nginx/sites-available/react-app /etc/nginx/sites-enabled/
+
+sudo nginx -t
+sudo systemctl restart nginx
+
+
+#################
+# Install Moodle
+#################
+cd moodle || exit 1
+docker-compose build
+docker-compose up -d
+
+cd - || exit 1
+
+
+####################
+# Install react-app
+####################
+cd website/docker || exit 1
+docker-compose up --build -d
+cd - || exit 1
